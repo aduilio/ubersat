ea85d9c2fe30862a8a8af59dbb56b7c58925e936
Aduilio Silva <developer.aduilio@gmail.com>
2022-05-25 10:47:49 -0300

    Select state
    Option to select a different state to search plans.

--- app/src/main/java/com/aduilio/viasat/ubersat/activity/MainActivity.kt
+++ app/src/main/java/com/aduilio/viasat/ubersat/activity/MainActivity.kt
@@ -2,8 +2,11 @@ package com.aduilio.viasat.ubersat.activity
 
 import android.location.Location
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.aduilio.viasat.ubersat.R
 import com.aduilio.viasat.ubersat.adapter.PlanAdapter
@@ -13,6 +16,7 @@ import com.aduilio.viasat.ubersat.helper.LocationHelper
 import com.aduilio.viasat.ubersat.viewmodel.AdminAreaViewModel
 import com.aduilio.viasat.ubersat.viewmodel.PlanViewModel
 import com.google.android.material.snackbar.Snackbar
+
 
 class MainActivity : AppCompatActivity(), LocationHelper.LocationHelperListener {
 
@@ -31,6 +35,21 @@ class MainActivity : AppCompatActivity(), LocationHelper.LocationHelperListener 
         setupViewModel()
 
         locationHelper = LocationHelper(this, this)
+        binding.tvNoPlans.visibility = View.GONE
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.plans_menu, menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_change_state -> showStateDialog()
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 
     override fun locationChange(location: Location) {
@@ -46,6 +65,12 @@ class MainActivity : AppCompatActivity(), LocationHelper.LocationHelperListener 
     private fun setupViewModel() {
         planViewModel.plans.observe(this) {
             planAdapter.setPlans(it)
+
+            if (it.isEmpty()) {
+                binding.tvNoPlans.visibility = View.VISIBLE
+            } else {
+                binding.tvNoPlans.visibility = View.GONE
+            }
         }
 
         planViewModel.showProgress.observe(this) { show ->
@@ -82,5 +107,25 @@ class MainActivity : AppCompatActivity(), LocationHelper.LocationHelperListener 
             Snackbar.make(binding.root, R.string.fail_get_plans, Snackbar.LENGTH_SHORT)
                 .show()
         }
+    }
+
+    private fun showStateDialog() {
+        val states = AdminAreaViewModel.getStates()
+
+        AlertDialog.Builder(this)
+            .setSingleChoiceItems(states, -1, null)
+            .setPositiveButton(
+                R.string.search
+            ) { dialog, _ ->
+                val selectedPosition: Int =
+                    (dialog as AlertDialog).listView.checkedItemPosition
+                if (selectedPosition >= 0) {
+                    val adminAreaName = states[selectedPosition].toString()
+                    val adminAreaCode = AdminAreaViewModel.getAdminAreaCode(adminAreaName)
+                    getPlans(AdminArea(adminAreaName, adminAreaCode!!))
+                }
+            }
+            .setNegativeButton(R.string.cancel, null)
+            .show()
     }
 }
--- app/src/main/java/com/aduilio/viasat/ubersat/viewmodel/AdminAreaViewModel.kt
+++ app/src/main/java/com/aduilio/viasat/ubersat/viewmodel/AdminAreaViewModel.kt
@@ -78,5 +78,37 @@ class AdminAreaViewModel() : ViewModel() {
                 else -> null
             }
         }
+
+        fun getStates(): Array<CharSequence> {
+            return arrayOf(
+                "Acre",
+                "Alagoas",
+                "Amapá",
+                "Amazonas",
+                "Bahia",
+                "Ceará",
+                "Distrito Federal",
+                "Espírito Santo",
+                "Goiás",
+                "Maranhão",
+                "Mato Grosso",
+                "Mato Grosso do Sul",
+                "Minas Gerais",
+                "Pará",
+                "Paraíba",
+                "Paraná",
+                "Pernambuco",
+                "Piauí",
+                "Rio de Janeiro",
+                "Rio Grande do Norte",
+                "Rio Grande do Sul",
+                "Rondônia",
+                "Roraima",
+                "Santa Catarina",
+                "São Paulo",
+                "Sergipe",
+                "Tocantins"
+            )
+        }
     }
 }
